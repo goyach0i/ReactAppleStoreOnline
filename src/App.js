1f3d25304fb951bc67f361b4fbd04638ff9d18fe
goyach0i <dev.goyachoi@gmail.com>
2024-12-19 23:23:52 +0900

    react-slick install

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './App.css';
 
 const Header = () => {
